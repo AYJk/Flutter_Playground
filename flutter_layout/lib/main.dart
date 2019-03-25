256cf4c4c030640ff76af6e2859b1f853afce07c
AYJk <andy5759520@gmail.com>
2019-03-25 23:42:19 +0800

    commit

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -5,31 +5,92 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Column buildButtonColum(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: new Text(label,
+                style: new TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w400,
+                  color: color,
+                )),
+          ),
+        ],
+      );
+    }
+
     Widget titleSection = new Container(
-      color: Colors.red,
       padding: const EdgeInsets.all(32.0),
       child: new Row(
         children: <Widget>[
-          new Column(
-            children: <Widget>[
-              new Text("Oeschinen Lake Campground")
-            ],
-          )
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text('Oeschinen Lake Campground',
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                new Text(
+                  'Kandersteg, Switzerland',
+                  style: new TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41')
         ],
       ),
     );
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColum(Icons.call, 'CALL'),
+          buildButtonColum(Icons.near_me, 'ROUTE'),
+          buildButtonColum(Icons.share, 'SHARE')
+        ],
+      ),
+    );
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
     return new MaterialApp(
       title: 'Flutter Layout',
-      theme: new ThemeData(
-        primaryColor: Colors.blue
-      ),
+      theme: new ThemeData(primaryColor: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Top Lakes'),
         ),
-        body: Column(
+        body: new ListView(
           children: <Widget>[
-            titleSection
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
           ],
         ),
       ),
